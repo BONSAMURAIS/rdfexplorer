66a6976473df1c3bcfdaff071fe843d26d7bb401
Monzyy <mnha@cs.aau.dk>
2022-01-12 17:20:51 +0100

    Fix flask config

--- rdfexplorer/__init__.py
+++ rdfexplorer/__init__.py
@@ -6,10 +6,10 @@ from config import Config
 
 def create_app(config_object=Config):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
+    app.config.from_object(config_object)
 
     app.register_blueprint(routes.bp)
-    app.config.config_object(config_object)
 
     # a simple page that says hello
     @app.route('/hello')
