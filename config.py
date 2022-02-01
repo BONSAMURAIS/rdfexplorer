9610e167f4cbc2cc0236b03aa404cf6fd051189d
Monzyy <mnha@cs.aau.dk>
2022-02-01 10:46:44 +0100

    Different queries for graphs and nodes

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ class Config:
     SECRET_KEY = env.str('SECRET_KEY', 'dev')
     RDF_DATA_DIR = env.path('RDF_DATA_DIR')
     SPARQL_HOST = env.str('SPARQL_HOST')
+    DOMAIN_NAME = env.str('DOMAIN_NAME')
